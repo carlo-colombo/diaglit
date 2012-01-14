304726c7385b1b4bec817164e678267e9a8166be
Carlo Colombo <carlo.colombo@gmx.com>
2012-01-14 18:50:32 +0100

    example page

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
-(function() {
-	var diaglit = require('diaglit');
-})()
+(function($, _) {
+	$.domReady(function() {
+		var diaglit = require('diaglit');
+
+		_($('.dialog-definition')).each(function(el) {
+			var $el = $(el),
+				d = diaglit(JSON.parse($el.text()));
+			
+			$el.previous()
+				.attr('data-controls-modal',d.$dialog.attr('id'))
+		})
+	})
+})(ender, require('underscore'))
