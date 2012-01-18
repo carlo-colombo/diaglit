360510864baa2b6e124ad14aacdd38b271581265
Carlo Colombo <carlo.colombo@gmx.com>
2012-01-18 19:50:47 +0100

    merged from master to publish tests

---
+++ src/diaglit.js
@@ -0,0 +1,86 @@
+!function(name, context, definition) {
+	if (typeof module !== 'undefined') module.exports = definition(ender);
+	else if (typeof define === 'function' && typeof define.amd === 'object') define(ender);
+	else context[name] = definition(ender);
+}('diaglit', this, function($) {
+	return function(dialog, options) {
+
+		var _ = require('underscore'),
+			controls = require('diaglit.controls'),
+			_diaglit = {},
+			li = _.template('\
+				<li>\
+					<a href="<%= href %>"><%= label %></a>\
+				</li>\
+			') //li template definition
+		if (_.isFunction(options)) {
+			options = {
+				'onSubmit': options
+			}
+		}
+		options = _.defaults(options || {}, {
+			appendTo: 'body',
+			'data': {}
+		})
+
+		//dialog skeleton
+		_diaglit.$dialog = $(_.template('\
+			<div id="<%= id %>" title="<%= title %>" class="modal">\
+				<div class="modal-header">\
+		            <a href="#" class="close">x</a>\
+		            <h3><%= title %></h3>\
+		        </div>\
+		        <div class="modal-body">\
+		        	<ul class="tabs" data-tabs="tabs"></ul>\
+		        	<form class="tab-content"></form>\
+	        	</div>\
+	        	<div class="modal-footer">\
+		            <a href="#" class="btn secondary cancel">Cancel</a>\
+		            <a href="#" class="btn primary save">Save</a>\
+		         </div>\
+			</div>\
+		', {
+			id: _.uniqueId('dialog_' + dialog.title.replace(' ', '')),
+			title: dialog.title,
+		})).hide().appendTo(options.appendTo).modal({
+			backdrop: true
+		})
+
+		//tabs and fieldset appending
+		_(dialog.tabs).map(function(v, k) {
+			//iterate through tabs and generate <li> and <fieldset>
+			var tabId = _.uniqueId(k)
+			return [
+			$(li({
+				href: '#' + tabId,
+				label: v.label || k
+			})), _.reduce(v.fields, function($fieldset, field) {
+				//generate fields and append to field
+				return $fieldset.append(controls.field(field, options['data']))
+			}, $('<fieldset>').attr({
+				'id': tabId,
+				'class': 'tab-pane'
+			}))]
+		}).reduce(function(memo, tab) { // [(li,fieldset),(li,fieldset),...]
+			$(memo[0]).append(tab[0]); //ul += li
+			$(memo[1]).append(tab[1]); //form += fieldset
+			return memo
+		}, _diaglit.$dialog.find('ul,form')); //memo
+
+		(function(d) {
+			d.find('li:nth-child(1),fieldset:nth-child(1)').addClass('active')
+			d.find('.btn.cancel').bind('click', function(e) {
+				e.stop()
+				d.modal('hide')
+			})
+			if (options['onSubmit']) {
+				d.find('.btn.save').bind('click', options['onSubmit'])
+				if (options['hideOnSubmit']) {
+					d.modal('close')
+				}
+			}
+		})(_diaglit.$dialog);
+
+		return _diaglit;
+	}
+});
---
+++ test/test.js
@@ -0,0 +1,103 @@
+var expect = require('chai').expect
+ender.domReady(function() {
+	describe('diaglit', function() {
+		var diaglit, d, _ = require('underscore'),
+			dialog1 = {
+				"tabs": {
+					"tab1": {
+						"label": "Tab 1",
+						"fields": [{
+							"name": "title",
+							"type": "text",
+							"placeholder": "Insert name here",
+							"label": "Character name"
+						}, {
+							"name": "characterClass",
+							"type": "text",
+							"label": "class"
+						}, {
+							"name": "level",
+							"type": "number",
+							"max": 30,
+							"min": 1,
+							"value": 1
+						}]
+					},
+					"tab2": {
+						"label": "Tab 2",
+						"fields": [{
+							"name": "select",
+							"type": "select",
+							"options": ["option 1", "option 2"]
+						}]
+					}
+				},
+				"title": "Properties"
+			}
+
+		it('should be exposed', function() {
+			diaglit = require('diaglit');
+		})
+
+		it('should not throws any error', function() {
+			d = diaglit(dialog1)
+		})
+
+		it('should accept config object as 2nd argument', function() {
+			d = diaglit(dialog1, {});
+		})
+
+		it('should accept callback function as 2nd argument', function() {
+			d = diaglit(dialog1, function() {})
+		})
+
+		describe('when created', function() {
+			it('should have a Modal istance as attribute data-modal', function() {
+				var d = require('diaglit')(dialog1)
+				expect(d.$dialog.data('modal')).to.be.an.instanceof(ender.fn.modal.Modal)
+			})
+
+			it('should initialize dialog using data option', function() {
+				var d = require('diaglit')(dialog1, {
+					data: {
+						'title': 'overriding title',
+						'level': 30
+					}
+				}),
+					title = d.$dialog.find('[name=title]'),
+					level = d.$dialog.find('[name=level]')
+
+					expect(title.attr('value')).to.be.equal('overriding title')
+					expect(level.attr('value')).to.be.equal('30')
+
+					d.$dialog.modal('show')
+			})
+		})
+
+		describe('pluggable controls', function() {
+			it('should accept additional control', function() {
+				var c = require('diaglit.controls');
+
+				c['custom'] = function() {
+					return $('<span class="kustom">').text('kustom Kontrol')
+				}
+
+				var d = require('diaglit')({
+					title: 'kustomized control',
+					tabs: {
+						tab: {
+							label: 'tab',
+							fields: [{
+								type: 'custom',
+								name: 'Kustom'
+							}]
+						}
+					}
+				})
+
+				expect(d.$dialog.find('.kustom')).to.be.not.empty
+				expect(d.$dialog.find('.kustom').is('span')).to.be.true
+			})
+		})
+	})
+})
